6fc513c713769e137776b415a6b9c039b7a9b50a
12Mateadamadze <159064219+12Mateadamadze@users.noreply.github.com>
2024-03-27 22:43:57 +0400

    fifth commit

---
+++ goa/day12/classwork/cw.py
@@ -0,0 +1,13 @@
+# print(30 < 25)
+# print(5 < 9)
+# print(50 > 100)
+
+
+
+#true/false = on/off = 1/0
+
+#binary code = ორობითი სისტემა = 1/0
+
+#string--integer--float--boolean
+
+
---
+++ goa/day12/homework/hw.py
@@ -0,0 +1,15 @@
+#მომხმარებელს შემოატანინეთ 4 სხვადასხვა ტიპის მონაცემი (string,integer,float,boolean) შემდეგ დაბეჭდეთ ეს შემოტანილი მონაცემები და მათი მონაცემთა ტიპები
+
+
+
+best = input("string: ")
+number = int(input("integer: "))
+fraction = float(input("float: "))
+comparison = bool(input("boolean: "))
+
+print(type(best))
+print(type(number))
+print(type(fraction))
+print(type(comparison))
+
+
